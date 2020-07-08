ade01ae260cfc4f76a7776c9e03612031f877945
Shuhei-Kubo <shuhei.k6221@gmail.com>
2020-07-08 15:18:40 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '<18.178.181.173>', user: 'ec2-user', roles: %w{app db web}
+server '18.178.181.173', user: 'ec2-user', roles: %w{app db web}
